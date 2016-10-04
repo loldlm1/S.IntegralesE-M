d9ee1258b07dcdb8d221f8e0d3d5e1b630cce8c6
mtorres8969 <loldlm1@gmail.com>
2016-10-04 16:42:19 -0400

    header v0.5

--- app/assets/javascripts/integral_services.js
+++ app/assets/javascripts/integral_services.js
@@ -1,2 +1,34 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+integralServicesJS = {
+  
+  productos: function() {
+    $("#productos").append(
+      '<br>',
+      '<span class="glyphicon glyphicon-wrench nav-icon" aria-hidden="true"></span>'
+    );
+  },
+  sistema: function() {
+    $("#sistema").append(
+      '<br>',
+      '<span class="glyphicon glyphicon-phone nav-icon" aria-hidden="true"></span>'
+    );
+  },
+  telecomunicacion: function() {
+    $("#telecomunicacion").append(
+      '<br>',
+      '<span class="glyphicon glyphicon-signal nav-icon" aria-hidden="true"></span>'
+    );
+  },
+  nosotros: function() {
+    $("#nosotros").append(
+      '<br>',
+      '<span class="glyphicon glyphicon-user nav-icon" aria-hidden="true"></span>'
+    );
+  },
+  iconRemove: function() {
+    $("#productos").children().remove();
+    $("#sistema").children().remove();
+    $("#telecomunicacion").children().remove();
+    $("#nosotros").children().remove();
+  }
+
+}
